bce67da87d632dc4f262e4fbfb2ff0289d63d690
Ricardo da Silva Lima <rslima@gmail.com>
2022-08-16 19:42:45 -0300

    fix: Corrige anotações no Controller

--- src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
@@ -1,9 +1,11 @@
 package br.gov.lexml.editoremendas;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class EditorApiController {
     @GetMapping
     public String hello() {
