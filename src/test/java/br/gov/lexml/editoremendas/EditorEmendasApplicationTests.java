2313c0c85b4a53ed719d1b2e7c15cf5eb8bbaf74
Ricardo da Silva Lima <rslima@gmail.com>
2022-08-16 19:28:23 -0300

    feat: Cria app Spring Boot

---
+++ src/test/java/br/gov/lexml/editoremendas/EditorEmendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.lexml.editoremendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorEmendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
