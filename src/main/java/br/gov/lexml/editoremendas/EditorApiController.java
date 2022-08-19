74a1b6b7ec3eaf842b6122bd00383d27221343f1
Ricardo da Silva Lima <rslima@gmail.com>
2022-08-19 13:12:23 -0300

    feat: geração do pdf

--- src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
@@ -1,14 +1,34 @@
 package br.gov.lexml.editoremendas;
 
+import br.gov.lexml.eta.etaservices.emenda.Emenda;
+import br.gov.lexml.eta.etaservices.printing.pdf.PdfGenerator;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class EditorApiController {
+
+    private final PdfGenerator pdfGenerator;
+
+    public EditorApiController(PdfGenerator pdfGenerator) {
+        this.pdfGenerator = pdfGenerator;
+    }
+
     @GetMapping
     public String hello() {
         return "Hello";
+    }
+
+    @PostMapping(produces = "application/pdf")
+    public byte[] salvaEmenda(Emenda emenda) throws IOException {
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+        pdfGenerator.generate(emenda, os);
+        return os.toByteArray();
     }
 }
