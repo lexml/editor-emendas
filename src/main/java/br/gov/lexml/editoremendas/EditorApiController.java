dcc032ab4e655030f74e2ff3c36778258c5ef561
Ricardo da Silva Lima <rslima@gmail.com>
2022-08-20 07:41:13 -0300

    fix: corrige parâmetro errado

--- src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
@@ -1,9 +1,12 @@
 package br.gov.lexml.editoremendas;
 
-import br.gov.lexml.eta.etaservices.emenda.Emenda;
+import br.gov.lexml.eta.etaservices.printing.json.EmendaPojo;
 import br.gov.lexml.eta.etaservices.printing.pdf.PdfGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,8 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api")
 public class EditorApiController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EditorApiController.class);
 
     private final PdfGenerator pdfGenerator;
 
@@ -26,7 +31,8 @@ public class EditorApiController {
     }
 
     @PostMapping(produces = "application/pdf")
-    public byte[] salvaEmenda(Emenda emenda) throws IOException {
+    public byte[] salvaEmenda(@RequestBody final EmendaPojo emenda) throws IOException {
+        LOGGER.info("emenda: {}", emenda);
         final ByteArrayOutputStream os = new ByteArrayOutputStream();
         pdfGenerator.generate(emenda, os);
         return os.toByteArray();
