536ff04aacf249c4378aef7b9c6e37d0bd8ba418
arthurcordeiro <arthur.cordeiro@gmail.com>
2022-08-24 11:44:53 -0300

    feat: cria ícone de expandir ementa no detalhe da emenda

--- jsapp/src/components/edt-app.ts
+++ jsapp/src/components/edt-app.ts
@@ -95,12 +95,29 @@ export class EdtApp extends LitElement {
             href="#"
             onclick="document.querySelector('.dialog-emenda').show();"
             class="detalhe-emenda--titulo"
+            aria-label="Expandir ementa"
+            title="Expandir ementa"
           >
             <span class="detalhe-emenda--nome-proposicao">
               ${this.proposicao.nomeProposicao} -
             </span>
             <span class="detalhe-emenda--ementa">
               ${unsafeHTML(this.proposicao.ementa)}
+            </span>
+            <span aria-label="Expandir ementa" title="Expandir ementa">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-arrows-angle-expand"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"
+                />
+              </svg>
             </span>
           </a>
           <div>
