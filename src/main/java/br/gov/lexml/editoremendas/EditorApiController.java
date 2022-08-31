e622f73fd9789d46225eefdf95dfd6aee187884a
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2022-08-31 17:29:48 -0300

    feat: serviço pdf to json
    Refs #1

--- src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorApiController.java
@@ -1,18 +1,25 @@
 package br.gov.lexml.editoremendas;
 
-import br.gov.lexml.eta.etaservices.printing.json.EmendaPojo;
-import br.gov.lexml.eta.etaservices.printing.pdf.PdfGenerator;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import br.gov.lexml.eta.etaservices.emenda.EmendaJsonGenerator;
+import br.gov.lexml.eta.etaservices.printing.json.EmendaPojo;
+import br.gov.lexml.eta.etaservices.printing.pdf.PdfGenerator;
 
 @RestController
 @RequestMapping("/api")
@@ -22,9 +29,12 @@ public class EditorApiController {
     private static final Logger LOGGER = LoggerFactory.getLogger(EditorApiController.class);
 
     private final PdfGenerator pdfGenerator;
-
-    public EditorApiController(PdfGenerator pdfGenerator) {
+    
+    private final EmendaJsonGenerator jsonGenerator;
+    
+    public EditorApiController(PdfGenerator pdfGenerator, EmendaJsonGenerator jsonGenerator) {
         this.pdfGenerator = pdfGenerator;
+        this.jsonGenerator = jsonGenerator;
     }
 
     @GetMapping
@@ -32,11 +42,23 @@ public class EditorApiController {
         return "Hello";
     }
 
-    @PostMapping(produces = "application/pdf")
-    public byte[] salvaEmenda(@RequestBody final EmendaPojo emenda) throws IOException {
+    @PostMapping(path = "/emenda/json2pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public void salvaEmenda(@RequestBody final EmendaPojo emenda, HttpServletResponse response) throws IOException {
         LOGGER.info("emenda: {}", emenda);
-        final ByteArrayOutputStream os = new ByteArrayOutputStream();
-        pdfGenerator.generate(emenda, os);
-        return os.toByteArray();
+        pdfGenerator.generate(emenda, response.getOutputStream());
     }
+    
+//    @PostMapping(path = "/emenda/pdf2json", consumes = MediaType.APPLICATION_OCTET_STREAM_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public void abreEmenda(@RequestBody final String pdfBase64, HttpServletResponse response) throws Exception {
+//    	response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//    	byte[] pdfBytes = Base64.getDecoder().decode(pdfBase64.getBytes());
+//        jsonGenerator.extractJsonFromPdf(new ByteArrayInputStream(pdfBytes), response.getWriter());
+//    }
+    
+    @PostMapping(path = "/emenda/pdf2json", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void abreEmenda(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws Exception {
+    	response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    	jsonGenerator.extractJsonFromPdf(file.getInputStream(), response.getWriter());
+    }
+    
 }
--- src/main/java/br/gov/lexml/editoremendas/EmendaXmlConfiguration.java
+++ src/main/java/br/gov/lexml/editoremendas/EmendaXmlConfiguration.java
@@ -1,12 +1,15 @@
 package br.gov.lexml.editoremendas;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import br.gov.lexml.eta.etaservices.emenda.EmendaJsonGenerator;
+import br.gov.lexml.eta.etaservices.emenda.EmendaJsonGeneratorBean;
 import br.gov.lexml.eta.etaservices.parsing.xml.EmendaXmlUnmarshaller;
 import br.gov.lexml.eta.etaservices.printing.pdf.PdfGenerator;
 import br.gov.lexml.eta.etaservices.printing.pdf.PdfGeneratorBean;
 import br.gov.lexml.eta.etaservices.printing.pdf.VelocityTemplateProcessorFactory;
 import br.gov.lexml.eta.etaservices.printing.xml.EmendaXmlMarshaller;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EmendaXmlConfiguration {
@@ -28,5 +31,10 @@ public class EmendaXmlConfiguration {
     @Bean
     PdfGenerator pdfGenerator(VelocityTemplateProcessorFactory templateProcessorFactory, EmendaXmlMarshaller emendaXmlMarshaller) {
         return new PdfGeneratorBean(templateProcessorFactory, emendaXmlMarshaller);
+    }
+    
+    @Bean
+    EmendaJsonGenerator emendaJsonGenerator() {
+    	return new EmendaJsonGeneratorBean();
     }
 }
