e7571538d15bfbb48d8dc92dcb5fcac10c124a4e
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2022-09-09 15:56:59 -0300

    fix: visualização do pdf e icones

--- src/main/java/br/gov/lexml/editoremendas/EditorEmendasApplication.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorEmendasApplication.java
@@ -2,9 +2,6 @@ package br.gov.lexml.editoremendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EditorEmendasApplication {
@@ -14,9 +11,4 @@ public class EditorEmendasApplication {
 		SpringApplication.run(EditorEmendasApplication.class, args);
 	}
 	
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}	
-
 }
