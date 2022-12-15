52233ca003935edbc2f08913281ad719f2e3df97
marco <marco@frago-vostro>
2022-12-15 15:48:54 -0300

    fix: configura executável de acordo com SO em runtime
    refs #124

--- src/main/java/br/gov/lexml/editoremendas/EditorEmendasApplication.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorEmendasApplication.java
@@ -1,13 +1,19 @@
 package br.gov.lexml.editoremendas;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EditorEmendasApplication {
-
+	
 	public static void main(String[] args) {
 		System.setProperty("java.net.useSystemProxies", "true");
+		if (SystemUtils.IS_OS_WINDOWS) {
+			System.setProperty("lexml-jsonix.cli", "jsonix-lexml-win.exe");
+		} else {
+			System.setProperty("lexml-jsonix.cli", "./jsonix-lexml-linux");
+		}
 		SpringApplication.run(EditorEmendasApplication.class, args);
 	}
 	
