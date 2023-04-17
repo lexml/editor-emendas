eca9fc9cb4489d742222dd3683003eeb7a7f4ac0
Marcos Fragomeni <marcos.fragomeni@gmail.com>
2023-04-17 19:00:38 -0300

    build: remove service workers
    refs #166

--- jsapp/rollup.config.js
+++ jsapp/rollup.config.js
@@ -3,8 +3,6 @@ import babel from '@rollup/plugin-babel';
 import html from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import { terser } from 'rollup-plugin-terser';
-import { generateSW } from 'rollup-plugin-workbox';
-import path from 'path';
 import copy from 'rollup-plugin-copy';
 
 export default {
@@ -21,9 +19,7 @@ export default {
   plugins: [
     /** Enable using HTML as rollup entrypoint */
     html({
-      minify: true,
-      injectServiceWorker: true,
-      serviceWorkerPath: 'dist/sw.js',
+      minify: true
     }),
     /** Resolve bare module imports */
     nodeResolve(),
@@ -66,20 +62,6 @@ export default {
           },
         ],
       ],
-    }),
-    /** Create and inject a service worker */
-    generateSW({
-      globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
-      navigateFallback: '/index.html',
-      // where to output the generated sw
-      swDest: path.join('dist', 'sw.js'),
-      // directory to match patterns against to be precached
-      globDirectory: path.join('dist'),
-      // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest}'],
-      skipWaiting: true,
-      clientsClaim: true,
-      runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
     }),
     copy({
       targets: [
