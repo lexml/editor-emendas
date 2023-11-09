63e1f58b89f38a07dc71cfc34a6ec989215db508
fecocao <filipeedusoares@gmail.com>
2023-11-09 13:35:36 -0300

    test: cria teste unitário para a classe objeto-util

--- jsapp/src/utils/objeto-util.ts
+++ jsapp/src/utils/objeto-util.ts
@@ -3,7 +3,6 @@ const isNullOrUndefined = (obj: any): boolean => {
 };
 
 export const objetosIguais = (obj1: any, obj2: any): boolean => {
-  // Verifica se ambos são objetos
   if (isNullOrUndefined(obj1) && isNullOrUndefined(obj2)) {
     return true;
   }
@@ -12,7 +11,6 @@ export const objetosIguais = (obj1: any, obj2: any): boolean => {
     return false;
   }
 
-  // Obtém as chaves dos objetos
   let keys1 = Object.keys(obj1);
   let keys2 = Object.keys(obj2);
 
@@ -24,26 +22,21 @@ export const objetosIguais = (obj1: any, obj2: any): boolean => {
     keys2 = keys2.sort();
   }
 
-  // Verifica se o número de chaves é o mesmo
   if (keys1.length !== keys2.length) {
     return false;
   }
 
-  // Itera sobre as chaves e compara os valores
   for (const key of keys1) {
-    // Se o valor for um objeto, chama recursivamente a função de comparação
     if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
       if (!objetosIguais(obj1[key], obj2[key])) {
         return false;
       }
     } else {
-      // Compara os valores diretamente
       if (obj1[key] !== obj2[key]) {
         return false;
       }
     }
   }
 
-  // Se todas as comparações passaram, os objetos são iguais
   return true;
 };
