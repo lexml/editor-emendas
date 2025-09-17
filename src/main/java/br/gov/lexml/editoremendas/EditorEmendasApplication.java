d66a1258bc5f3ffe9c18ecbf75d98de6135491d8
João Matos <joaofmatos.10@gmail.com>
2025-09-17 18:52:10 -0300

    refs #428: Carrega configuração do lexeditweb

--- src/main/java/br/gov/lexml/editoremendas/EditorEmendasApplication.java
+++ src/main/java/br/gov/lexml/editoremendas/EditorEmendasApplication.java
@@ -1,11 +1,14 @@
 package br.gov.lexml.editoremendas;
 
 import org.apache.commons.lang3.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EditorEmendasApplication {
+    private static final Logger log = LoggerFactory.getLogger(EditorEmendasApplication.class);
 	
 	public static void main(String[] args) {
 		System.setProperty("java.net.useSystemProxies", "true");
@@ -16,6 +19,11 @@ public class EditorEmendasApplication {
 		} else {
 			System.setProperty("lexml-jsonix.cli", "./jsonix-lexml-linux");
 		}
+
+        if (System.getProperty("spring.profiles.active") == null) {
+            log.info("Using lexeditweb production url");
+            System.setProperty("LEXEDITWEB_URL", "https://legis.senado.leg.br/lexeditweb");
+        }
 		SpringApplication.run(EditorEmendasApplication.class, args);
 	}
 	
